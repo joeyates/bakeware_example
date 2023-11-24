f7f4bfae4100cfb0be4baafd09ba6af7ba4223e4
Joe Yates <joe.g.yates@gmail.com>
2023-11-24 16:14:09 +0100

    Configure the release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule BakewareExample.MixProject do
   use Mix.Project
 
+  @app :bakeware_example
+
   def project do
     [
-      app: :bakeware_example,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [{@app, release()}]
     ]
   end
 
@@ -20,6 +23,15 @@ defmodule BakewareExample.MixProject do
   defp deps do
     [
       {:bakeware, ">= 0.0.0", runtime: false},
+    ]
+  end
+
+  defp release do
+    [
+      overwrite: true,
+      quiet: true,
+      steps: [:assemble, &Bakeware.assemble/1],
+      strip_beams: Mix.env() == :prod
     ]
   end
 end
